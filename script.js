3f79816c8a86170a7bc4d191d7b09697966115dd
Solomon <laoberecumitica@gmail.com>
2025-07-25 21:49:50 +0300

    .

---
+++ script.js
@@ -0,0 +1,24 @@
+const characters = {
+  airgroove: {
+    personality: "",
+    speedpriority: "",
+    staminapriority: "",
+    powerpriority: "",
+    gutspriority: "",
+   intelligencepriority: "",
+   skill1: "",
+   skill2: "",
+   skill3: "",
+   besttotrain: "",  
+racestrategy: "", 
+bestdistance: "", 
+okdistance: "", 
+worstdistance: "", 
+track
+
+
+
+
+  },    
+  
+};
